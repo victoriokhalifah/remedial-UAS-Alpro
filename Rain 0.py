a2838ad2473793d8e49dd38203a6db1f851a5b9f
victoriokhalifah <149156921+victoriokhalifah@users.noreply.github.com>
2023-12-29 21:25:21 +0700

    Create Rain 0.py

---
+++ Rain 0.py
@@ -0,0 +1,21 @@
+#robot maju
+def maju():
+    move()
+    move()
+    move()
+def jalan():
+    move()
+    move()  
+    
+#robot bangun tembok   
+def bangun_tembok():
+    build_wall()
+    turn_left()
+    turn_left()
+ 
+maju()
+maju()
+bangun_tembok()
+maju()
+jalan()
+bangun_tembok()
